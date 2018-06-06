c988c529d03c1fa5b5f39064c57d30cb721f78be
Qifeng Zhou <zhouqf09@gmail.com>
2018-06-06 19:56:56 +0200

    Update LGB_Mengfei_S1_newtime.py

--- qifeng/LGB_Mengfei_S1_newtime.py
+++ qifeng/LGB_Mengfei_S1_newtime.py
@@ -536,4 +536,9 @@ print(ft.head(50))
 
 print("All Done.")
 
-
+[100]   train's rmse: 0.224534  valid's rmse: 0.226934
+[200]   train's rmse: 0.216626  valid's rmse: 0.221355
+[300]   train's rmse: 0.212257  valid's rmse: 0.219317
+[400]   train's rmse: 0.208769  valid's rmse: 0.218093
+[500]   train's rmse: 0.206086  valid's rmse: 0.217387
+[600]   train's rmse: 0.2036    valid's rmse: 0.216893
