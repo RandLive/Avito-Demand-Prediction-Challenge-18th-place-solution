50b28a522bf93b31bba27a2b8007dc6a8c12d283
yukiohnishi <yukimilk0116@gmail.com>
2018-06-18 17:35:22 +0900

    add shell script to generate oof features.

--- yuki/avito/src/all_oof.sh
+++ yuki/avito/src/all_oof.sh
@@ -2,6 +2,7 @@ python3 -u create_categorical_oof_features.py > logs/create_categorical_oof_feat
 python3 -u create_embedding_features.py > logs/create_embedding_features.log
 # python3 -u create_image_nn_postprocess.py > logs/create_image_nn_postprocess.log
 python3 -u create_nlp_features_group.py > logs/create_nlp_features_group.log
+python3 -u create_stemming.py > logs/create_stemming.log
 python3 -u create_nlp_features_stemmed.py > logs/create_nlp_features_stemmed.log
 python3 -u create_oof_classification_features.py > logs/create_oof_classification_features.log
 python3 -u create_predict_price.py > logs/create_predict_price.log
